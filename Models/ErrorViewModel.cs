4a84e28c934dcb85d5ef692e2d4c014404d33732
madmdx07 <mahessdhant@gmail.com>
2023-09-17 15:41:42 +0545

    Revert "Add project files."
    This reverts commit 0d6692c01c0a30c3e41091125d38607405b01fb7.

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace StudentRegistrationInCore.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
